7b66337458c41c60cfafc6c739dd858b2ebfeaee
jaison <jaisonthomasjacob@gmail.com>
2025-03-26 14:45:50 +0530

    axios

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://skillscale-sqip.onrender.com",
+  baseURL: (process.env.REACT_APP_API_BASE_URL || "https://skillscale-sqip.onrender.com") + "/api", // Added /api here
   headers: {
     "Content-Type": "application/json",
   },
